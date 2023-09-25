2751c34c9cd2b4560d2d810d852adcca5fd09d31
Wallaby <avocado34.131@gmail.com>
2023-09-26 04:48:22 +0900

    [Feat] WaitingRoom 매치 생성 로직 구현

--- ScoreBoy Watch App/Views/WaitingRoomView.swift
+++ ScoreBoy Watch App/Views/WaitingRoomView.swift
@@ -17,15 +17,22 @@ struct WaitingRoomView: View {
   // MARK: - Views
   
   var body: some View {
-      VStack(spacing: 16) {
-        Text(viewModel.entryCode)
-          .font(.system(size: 28, weight: .bold))
-          .foregroundStyle(Color.white)
-        
-        Text("상대 플레이어에게\n코드를 보여 주세요")
-          .font(.system(size: 15))
-          .foregroundStyle(Color.sbGray30)
-      }
+    VStack(spacing: 16) {
+      Text(viewModel.entryCode)
+        .font(.system(size: 28, weight: .bold))
+        .foregroundStyle(Color.white)
+      
+      Text("상대 플레이어에게\n코드를 보여 주세요")
+        .font(.system(size: 15))
+        .foregroundStyle(Color.sbGray30)
+    }
+    .onAppear {
+      viewModel.generateRoom()
+      viewModel.observeMatchState()
+    }
+    .onDisappear {
+      viewModel.removeRoom()
+    }
   }
 }
 
--- ScoreBoy Watch App/Views/WaitingRoomViewModel.swift
+++ ScoreBoy Watch App/Views/WaitingRoomViewModel.swift
@@ -6,17 +6,34 @@
 //
 
 import SwiftUI
+import Firebase
 
 final class WaitingRoomViewModel: ObservableObject {
   
   // MARK: - Properties
   
   @Published var entryCode = RandomCodeGenerator.generateRandomCode()
-  
+  private let ref = Database.database().reference()
   
   // MARK: - Methods
   
-  public func generateEntryCode() {
-//    entryCode =
+  public func generateRoom() {
+    ref.child(entryCode)
+      .child("test UserID")
+      .setValue(["score" : 0, "state" : 0])
+  }
+  
+  public func removeRoom() {
+    ref.child(entryCode)
+      .child("test UserID")
+      .removeValue()
+  }
+  
+  public func observeMatchState() {
+    ref.child(entryCode)
+      .child(entryCode)
+      .observe(.childAdded) { snapshot in
+        print("🔥 matched")
+      }
   }
 }
